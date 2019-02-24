056d686f2297866772955b7d19e3176638ddecae
Toni-Ty <antonique2479@yahoo.com>
2019-02-24 04:00:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -107,9 +107,15 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # holiday_hash.map do |
+  holiday_hash.map do |season, holiday|
+  holiday.map do |holidays, supplies|
+    if supplies == BBQ
+      return supplies 
+        
+  
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    end
+  end
 end
