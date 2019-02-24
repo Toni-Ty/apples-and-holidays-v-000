f3421b07a11f2e360037cee49598964aa2077566
Toni-Ty <antonique2479@yahoo.com>
2019-02-24 04:06:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -110,7 +110,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
   holiday.map do |holidays, supplies|
     # binding.pry
-    if supplies == BBQ
+    if supplies.include? ("BBQ")
       return supplies
 
 
